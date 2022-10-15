36ca7047be6f260c93d1842bb50fb755a4847e6f
Fernanda Alvarez <mariafernandalvarezramirez@gmail.com>
2022-10-14 20:56:34 -0500

    Fixing paths for images and removing comments

--- README.md
+++ README.md
@@ -9,15 +9,15 @@ _Disclaimer: "Circuiticket" se desarrolló como parte de un proyecto para Labora
 
 El primer prototipo se realizó en papel.  
 
-![prototipo realizado en papel y lápiz](images\prototipo-papel.PNG)
+![prototipo realizado en papel y lápiz](images/prototipo-papel.PNG)
 
 Después, fue necesario crear un prototipo en Figma.  
 
-![prototipo realizado en figma](images\prototipo-figma.PNG)
+![prototipo realizado en figma](images/prototipo-figma.PNG)
 
 En ambos casos, se recbió _feedback_ por parte de las compañeras del bootcamp. Algunas de las observaciones realizadas fueron hacer que ocupara menos espacio (cuando se mostró el espaciado en papel), cambiar el tipo de fuente del header para que estuviera más en línea con el branding del Foro, así como reducir el texto en el pie de página, al cual se añadió también el logotipo del lugar.  
 
 Al final quedó de la siguiente manera. 
-![projecto final](images\final.PNG)
-![tarjeta válida](images\valid.PNG)
-![tarjeta no válida](images\not-valid.PNG)
+![projecto final](images/final.PNG)
+![tarjeta válida](images/valid.PNG)
+![tarjeta no válida](images/not-valid.PNG)
--- src/validator.js
+++ src/validator.js
@@ -1,53 +1,36 @@
-// Acá van las dos funciones correspondientes para ocultar los dígitos de la tarjeta utilizando maskify y validar la tarjeta de crédito utilizando isValid
-
 const validator = {
     isValid: function (creditCardNumber) {
         let individualNumber = creditCardNumber.split("");
-        //console.log(individualNumber);
-        let reversedNumbers = individualNumber.reverse(); // .reverse() se encarga de invertir los valores
-        console.log(reversedNumbers);
+        let reversedNumbers = individualNumber.reverse();
         const numbersInNewPosition = reversedNumbers;
         for (let counting = 0; counting < numbersInNewPosition.length; counting++) {
-            if (counting % 2 !== 0) { // Obtener los números pares
-                let evenNumbers = (numbersInNewPosition[counting] * 2); // los números pares se multiplican por 2
+            if (counting % 2 !== 0) {
+                let evenNumbers = (numbersInNewPosition[counting] * 2);
                 numbersInNewPosition[counting] = evenNumbers;
             }
-            if (counting % 2 !== 0 && numbersInNewPosition[counting] > 9) { // Aquí se vuelven a obtener los números pares y si cumple con la condición de que es menor a 9
-                let reducedResult = (numbersInNewPosition[counting] - 9); // Si un número da dos dígitos y restamos 9, da la misma suma que daría si los sumamos por separado, ej. 13-9=4, 1+3=4 / 15-9=6, 1+5=6
+            if (counting % 2 !== 0 && numbersInNewPosition[counting] > 9) {
+                let reducedResult = (numbersInNewPosition[counting] - 9);
                 numbersInNewPosition[counting] = reducedResult;
             }
         }
-        console.log(numbersInNewPosition);
         let totalValue = 0;
         for (let counter = 0; counter < numbersInNewPosition.length; counter++) {
             totalValue += Number(numbersInNewPosition[counter]);
         }
-        console.log(totalValue);
         if (totalValue % 10 === 0) {
-            //console.log(true);
             return true;
-        } else {
-            //console.log(false)
-            return false;
-        }
+        } else return false;
     }
     ,
     maskify: function (creditCardNumber) {
-        let lastFourDigits = creditCardNumber.slice(-4); // .slice() selecciona SOLAMENTE los últimos cuatro dígitos, regresa una copia en un nuevo arreglo sin modificar el original
-        let dividedData = creditCardNumber.split(""); // .split() fragmenta un string, al no contener un separador específico selecciona cada cáracter por separado
-        /* Se obtiene cada carácter para poder utilizarlos en el ciclo 'for', su estructura requiere de una expresión inicial, una condición y una expresión final.
-        En este caso maskNumbers nos indica en qué punto comenzará a contar, inicia con el carácter en la posición 0, después se le indica que nuestro conteo en maskNumbers debe ser menor a la longitud de dividedData y maskNumber se incrementa hasta que se cumpla la condición anterior */
+        let lastFourDigits = creditCardNumber.slice(-4);
+        let dividedData = creditCardNumber.split("");
         for (let maskNumbers = 0; maskNumbers < dividedData.length; maskNumbers++) {
             // Si se cumple, se ejecutará lo que está dentro de estas llaves
             dividedData[maskNumbers] = dividedData[maskNumbers].replace(/./g, "#");
-            /* .replace() se encarga de reemplazar los datos.
-            El valor entre barras corresponde al dato a cubrir; el punto y la g forman parte de una expresión regular (definida como un patrón de búsqueda).
-            El punto es un comodín pues representa "todos los caracteres", mientras que la g es de global, lo que significa que busca en todo el contenido;
-            el valor después de la coma es con lo que se sustituirá */
         }
-        let allLooseData = dividedData.join(""); // .join(), como su nombre lo indica, se encarga de unir los elementos, usar un separador es opcional y aquí no se requiere
+        let allLooseData = dividedData.join("");
         let hiddenAndUnhiddenNumbers = allLooseData.slice(0, -4) + lastFourDigits; // A
-        //console.log(hiddenAndUnhiddenNumbers);
         return hiddenAndUnhiddenNumbers;
     }
 };
